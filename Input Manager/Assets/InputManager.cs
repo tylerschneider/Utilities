1960b66fd13fb336d1f47dfba769344543232421
tylerschneider <46699128+tylerschneider@users.noreply.github.com>
2019-09-05 10:27:08 -0600

    Added Input Manager project

---
+++ Input Manager/Assets/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+
+    public Text outText;
+    public InputField textIn;
+    public Button enterButton;
+
+    void Start()
+    {
+        outText.text = "";
+    }
+
+    public void EnterPressed()
+    {
+        outText.text = Utilities.ProcessText(textIn.text);
+    }
+}
